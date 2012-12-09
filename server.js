f078f40407e76fd4eaed65bf6c0f7db9df669c24
Drew Harry <drew.harry@gmail.com>
2012-12-09 16:49:26 -0500

    Clients connecting to the server now.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express'),
     program = require('commander'),
     os = require('os'),
+    sockjs_lib = require('sockjs'),
+    http = require('http'),
     _ = require('underscore')._;
     
 var app = express();
+var httpServer = http.createServer(app);
 
 program.version('0.1')
   .option('-p, --port <port>', 'Sets the port to bind to (default 8080)')
@@ -26,4 +29,17 @@ app.get('/game', function(req, res) {
   res.render("game.ejs", options);
 });
 
-app.listen(options.port);
+var sockjs = sockjs_lib.createServer();
+sockjs.installHandlers(httpServer, {prefix:"/sock"});
+
+httpServer.listen(options.port);
+
+
+sockjs.on('connection', function(conn) {
+  console.log("client connected");
+  
+  conn.on("close", function() {
+    console.log("client closed");
+  });
+})
+
